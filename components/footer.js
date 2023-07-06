3ec513bc6d8758e6e1912b14d9345d733bc41544
Way00007 <anyi.wang@simplic.ai>
2023-07-07 00:07:30 +0800

    footer

--- components/footer.js
+++ components/footer.js
@@ -15,9 +15,9 @@ export default function Footer() {
   return (
     <div className="relative">
       <Container>
-        <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5">
-          <div className="lg:col-span-2">
-            <div>
+        {/* <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 dark:border-trueGray-700 lg:grid-cols-5"> */}
+          {/* <div className="lg:col-span-2"> */}
+            {/* <div>
               {" "}
               <Link href="/" className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <Image
@@ -29,7 +29,7 @@ export default function Footer() {
                     />
                   <span>Simplic AI</span>
               </Link>
-            </div>
+            </div> */}
 
             {/* <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
               Nextly is a free landing page & marketing website
@@ -51,7 +51,7 @@ export default function Footer() {
                 />
               </a>
             </div> */}
-          </div>
+          {/* </div> */}
 
           {/* <div>
             <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
@@ -104,7 +104,7 @@ export default function Footer() {
               </a>
             </div>
           </div> */}
-        </div>
+        {/* </div> */}
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
           Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
@@ -124,7 +124,7 @@ export default function Footer() {
         </div>
       </Container>
       {/* Do not remove this */}
-      <Backlink />
+      {/* <Backlink /> */}
     </div>
   );
 }
